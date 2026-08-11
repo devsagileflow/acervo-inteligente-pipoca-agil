120f01368609f197cb202e2836bb92dbc0da0bd2
George Maia <georgemaiaf@gmail.com>
2026-08-11 14:00:26 -0300

    Fix API static asset path handling

--- apps/api/src/plugins/static.ts
+++ apps/api/src/plugins/static.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 export default fp(async (app: FastifyInstance) => {
   app.register(fastifyStatic, {
-    root: path.join(__dirname, "..", "..", "public"),
+    root: path.join(process.cwd(), "public"),
     prefix: "/public/",
   });
 });
